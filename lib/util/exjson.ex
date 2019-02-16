02d7831cb6260340f80d42da5aafcfbb72dbc56b
efg.river <efg.river@gmail.com>
2019-02-17 07:48:38 +0900

    exjson fix

--- lib/util/exjson.ex
+++ lib/util/exjson.ex
@@ -2,7 +2,7 @@ defmodule Aedmap.ExJson do
 
   def get(base_url, resource_path) do
     
-    case (base_url <> resource_path) |> HTTPoison.get! do
+    case (base_url <> resource_path)  |> HTTPoison.get! do
       %HTTPoison.Response{body: body, status_code: 200} ->  body |> Poison.decode!
       %HTTPoison.Error{reason: reason} -> raise "Oops, #{reason}"
     end
--- mix.exs
+++ mix.exs
@@ -42,6 +42,8 @@ defmodule Aedmap.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:httpoison, "~> 1.2.0"},
+      {:poison, "~> 3.1"},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
