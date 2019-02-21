f78ddcd79a8b5c4cbb312353f0d38725be8f1088
藤川英悟 <efg.river@gmail.com>
2019-02-21 09:08:20 +0900

    Support APP_HOST env config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 
 config :aedmap, AedmapWeb.Endpoint,
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  url: [host:  System.get_env("APP_HOST") || System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
 config :aedmap, Aedmap.Repo,
